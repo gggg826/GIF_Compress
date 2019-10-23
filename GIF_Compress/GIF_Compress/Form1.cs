a2bac168e2fa370468371a777e3b9703ac544e4b
gggg826 <liushida87@126.com>
2019-10-24 00:57:46 +0800

    Optimize code

--- GIF_Compress/GIF_Compress/Form1.cs
+++ GIF_Compress/GIF_Compress/Form1.cs
@@ -17,9 +17,10 @@ namespace GIF_Compress
     public partial class Form1 : Form
     {
         private List<string> mAllgif;
-        private int mTargetW, mTargetH;
+        private int mTargetW;
         private float mTargetFrameDelay;
         private string mSaveDir;
+        private string mCurrentSaveFilePath;
 
         public Form1()
         {
@@ -39,20 +40,19 @@ namespace GIF_Compress
         {
             listView1.Columns.Add("文件名", -2, HorizontalAlignment.Left);
             listView1.Columns[0].Width = listView1.Width;
+            SetTargetFrameCount();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(mAllgif == null)
+            if (mAllgif == null)
             {
                 return;
             }
 
-            MessageBox.Show(GetDelay(mAllgif[0]).ToString());
-            return;
-
             for (int i = 0, c = mAllgif.Count; i < c; i++)
             {
+                CheckSavePath(mAllgif[i]);
                 CompressGIF(mAllgif[i]);
             }
 
@@ -70,10 +70,10 @@ namespace GIF_Compress
             string[] gifs = (string[])e.Data.GetData(DataFormats.FileDrop, false);
 
             string fileName = string.Empty;
-            for (int i = 0,c = gifs.Length; i < c; i++)
+            for (int i = 0, c = gifs.Length; i < c; i++)
             {
                 fileName = gifs[i];
-                if(mAllgif.Contains(fileName))
+                if (mAllgif.Contains(fileName))
                 {
                     continue;
                 }
@@ -98,21 +98,18 @@ namespace GIF_Compress
 
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
-            if(!int.TryParse(textBox1.Text, out mTargetW) || mTargetW <= 0)
-            {
-                MessageBox.Show("请输入正整数!", "错误");
-            }
-        }
-
-        private void TextBox2_TextChanged(object sender, EventArgs e)
-        {
-            if (!int.TryParse(textBox1.Text, out mTargetH) || mTargetH <= 0)
+            if (!int.TryParse(textBox1.Text, out mTargetW) || mTargetW <= 0)
             {
                 MessageBox.Show("请输入正整数!", "错误");
             }
         }
 
         private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            SetTargetFrameCount();
+        }
+
+        private void SetTargetFrameCount()
         {
             mTargetFrameDelay = 1 / (float)trackBar1.Value;
             label4.Text = trackBar1.Value.ToString();
@@ -126,119 +123,126 @@ namespace GIF_Compress
 
 
 
-
-
-        private void CompressGIF(string gifInPath)
+        private void CheckSavePath(string sourcePath)
         {
-            //原图路径
-            //string imgPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\2.gif";
-
             //做一层保护，判断当前输出文件夹存在与否
             if (!Directory.Exists(mSaveDir))
             {
                 Directory.CreateDirectory(mSaveDir);
             }
-            string savePath = mSaveDir + Path.GetFileName(gifInPath);
-            if(File.Exists(savePath))
+            mCurrentSaveFilePath = mSaveDir + Path.GetFileName(sourcePath);
+            if (File.Exists(mCurrentSaveFilePath))
             {
-                File.Delete(savePath);
+                File.Delete(mCurrentSaveFilePath);
             }
+        }
 
+        private void CompressGIF(string sourcePath)
+        {
             //原图
-            Image img = Image.FromFile(gifInPath);
+            Image img = Image.FromFile(sourcePath);
+            int saveW, saveH;
             //宽不够指定值的不缩放
             if (img.Width > mTargetW)
             {
-                mTargetH = (int)((float)img.Height / img.Width * mTargetW);
-                //新图第一帧
-                Image new_img = new Bitmap(mTargetW, mTargetH);
-                //新图其他帧
-                Image new_imgs = new Bitmap(mTargetW, mTargetH);
-                //新图第一帧GDI+绘图对象
-                Graphics g_new_img = Graphics.FromImage(new_img);
-                //新图其他帧GDI+绘图对象
-                Graphics g_new_imgs = Graphics.FromImage(new_imgs);
-                //配置新图第一帧GDI+绘图对象
-                g_new_img.CompositingMode = CompositingMode.SourceCopy;//overdraw 覆盖、混合
-                g_new_img.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                g_new_img.PixelOffsetMode = PixelOffsetMode.HighQuality;
-                g_new_img.SmoothingMode = SmoothingMode.HighQuality;
-                g_new_img.Clear(Color.FromKnownColor(KnownColor.Transparent));
-                //配置其他帧GDI+绘图对象
-                g_new_imgs.CompositingMode = CompositingMode.SourceCopy;
-                g_new_imgs.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                g_new_imgs.PixelOffsetMode = PixelOffsetMode.HighQuality;
-                g_new_imgs.SmoothingMode = SmoothingMode.HighQuality;
-                g_new_imgs.Clear(Color.FromKnownColor(KnownColor.Transparent));
-                //遍历维数
-                foreach (Guid gid in img.FrameDimensionsList)
+                saveW = mTargetW;
+                saveH = (int)((float)img.Height / img.Width * saveW);
+            }
+            else
+            {
+                saveW = img.Width;
+                saveH = img.Height;
+            }
+
+            //新图第一帧
+            Image new_img = new Bitmap(saveW, saveH);
+            //新图第一帧GDI+绘图对象
+            Graphics g_new_img = Graphics.FromImage(new_img);
+            //配置新图第一帧GDI+绘图对象
+            g_new_img.CompositingMode = CompositingMode.SourceCopy;//overdraw 覆盖、混合
+            g_new_img.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g_new_img.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            g_new_img.SmoothingMode = SmoothingMode.HighQuality;
+            g_new_img.Clear(Color.FromKnownColor(KnownColor.Transparent));
+
+            //新图其他帧
+            Image new_imgs = new Bitmap(saveW, saveH);
+            //新图其他帧GDI+绘图对象
+            Graphics g_new_imgs = Graphics.FromImage(new_imgs);
+            //配置其他帧GDI+绘图对象
+            g_new_imgs.CompositingMode = CompositingMode.SourceCopy;
+            g_new_imgs.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g_new_imgs.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            g_new_imgs.SmoothingMode = SmoothingMode.HighQuality;
+            g_new_imgs.Clear(Color.FromKnownColor(KnownColor.Transparent));
+            //遍历维数
+            foreach (Guid gid in img.FrameDimensionsList)
+            {
+                //因为是缩小GIF文件所以这里要设置为Time
+                //如果是TIFF这里要设置为PAGE
+                FrameDimension f = FrameDimension.Time;
+                //获取总帧数
+                int count = img.GetFrameCount(f);
+                //保存标示参数
+                Encoder encoder = Encoder.SaveFlag;
+                //
+                EncoderParameters ep = null;
+                //图片编码、解码器
+                ImageCodecInfo ici = null;
+                //图片编码、解码器集合
+                ImageCodecInfo[] icis = ImageCodecInfo.GetImageDecoders();
+                //为 图片编码、解码器 对象 赋值
+                foreach (ImageCodecInfo ic in icis)
                 {
-                    //因为是缩小GIF文件所以这里要设置为Time
-                    //如果是TIFF这里要设置为PAGE
-                    FrameDimension f = FrameDimension.Time;
-                    //获取总帧数
-                    int count = img.GetFrameCount(f);
-                    //保存标示参数
-                    Encoder encoder = Encoder.SaveFlag;
-                    //
-                    EncoderParameters ep = null;
-                    //图片编码、解码器
-                    ImageCodecInfo ici = null;
-                    //图片编码、解码器集合
-                    ImageCodecInfo[] icis = ImageCodecInfo.GetImageDecoders();
-                    //为 图片编码、解码器 对象 赋值
-                    foreach (ImageCodecInfo ic in icis)
+                    if (ic.FormatID == ImageFormat.Gif.Guid)
                     {
-                        if (ic.FormatID == ImageFormat.Gif.Guid)
-                        {
-                            ici = ic;
-                            break;
-                        }
+                        ici = ic;
+                        break;
                     }
-                    //每一帧
-                    for (int c = 0; c < count; c++)
-                    {
-                        //选择由维度和索引指定的帧
-                        img.SelectActiveFrame(f, c);
-                        //第一帧
-                        if (c == 0)
-                        {
-                            //将原图第一帧画给新图第一帧
-                            g_new_img.DrawImage(img, new Rectangle(0, 0, mTargetW, mTargetH), new Rectangle(0, 0, img.Width, img.Height), GraphicsUnit.Pixel);
-                            //把振频和透明背景调色板等设置复制给新图第一帧
-                            for (int i = 0; i < img.PropertyItems.Length; i++)
-                            {
-                                new_img.SetPropertyItem(img.PropertyItems[i]);
-                            }
-                            ep = new EncoderParameters(1);
-                            //第一帧需要设置为MultiFrame
-                            ep.Param[0] = new EncoderParameter(encoder, (long)EncoderValue.MultiFrame);
-                            //保存第一帧
-                            new_img.Save(savePath, ici, ep);
-                        }
-                        //其他帧
-                        else
-                        {
-                            //把原图的其他帧画给新图的其他帧
-                            g_new_imgs.DrawImage(img, new Rectangle(0, 0, mTargetW, mTargetH), new Rectangle(0, 0, img.Width, img.Height), GraphicsUnit.Pixel);
-                            //把振频和透明背景调色板等设置复制给新图第一帧
-                            for (int i = 0; i < img.PropertyItems.Length; i++)
-                            {
-                                new_imgs.SetPropertyItem(img.PropertyItems[i]);
-                            }
-                            ep = new EncoderParameters(1);
-                            //如果是GIF这里设置为FrameDimensionTime
-                            //如果为TIFF则设置为FrameDimensionPage
-                            ep.Param[0] = new EncoderParameter(encoder, (long)EncoderValue.FrameDimensionTime);
-                            //向新图添加一帧
-                            new_img.SaveAdd(new_imgs, ep);
-                        }
-                    }
-                    ep = new EncoderParameters(1);
-                    //关闭多帧文件流
-                    ep.Param[0] = new EncoderParameter(encoder, (long)EncoderValue.Flush);
-                    new_img.SaveAdd(ep);
                 }
+                //每一帧
+                for (int c = 0; c < count; c++)
+                {
+                    //选择由维度和索引指定的帧
+                    img.SelectActiveFrame(f, c);
+                    //第一帧
+                    if (c == 0)
+                    {
+                        //将原图第一帧画给新图第一帧
+                        g_new_img.DrawImage(img, new Rectangle(0, 0, saveW, saveH), new Rectangle(0, 0, img.Width, img.Height), GraphicsUnit.Pixel);
+                        //把振频和透明背景调色板等设置复制给新图第一帧
+                        for (int i = 0; i < img.PropertyItems.Length; i++)
+                        {
+                            new_img.SetPropertyItem(img.PropertyItems[i]);
+                        }
+                        ep = new EncoderParameters(1);
+                        //第一帧需要设置为MultiFrame
+                        ep.Param[0] = new EncoderParameter(encoder, (long)EncoderValue.MultiFrame);
+                        //保存第一帧
+                        new_img.Save(mCurrentSaveFilePath, ici, ep);
+                    }
+                    //其他帧
+                    else
+                    {
+                        //把原图的其他帧画给新图的其他帧
+                        g_new_imgs.DrawImage(img, new Rectangle(0, 0, saveW, saveH), new Rectangle(0, 0, img.Width, img.Height), GraphicsUnit.Pixel);
+                        //把振频和透明背景调色板等设置复制给新图第一帧
+                        //for (int i = 0; i < img.PropertyItems.Length; i++)
+                        //{
+                        //    new_imgs.SetPropertyItem(img.PropertyItems[i]);
+                        //}
+                        ep = new EncoderParameters(1);
+                        //如果是GIF这里设置为FrameDimensionTime
+                        //如果为TIFF则设置为FrameDimensionPage
+                        ep.Param[0] = new EncoderParameter(encoder, (long)EncoderValue.FrameDimensionTime);
+                        //向新图添加一帧
+                        new_img.SaveAdd(new_imgs, ep);
+                    }
+                }
+                ep = new EncoderParameters(1);
+                //关闭多帧文件流
+                ep.Param[0] = new EncoderParameter(encoder, (long)EncoderValue.Flush);
+                new_img.SaveAdd(ep);
 
                 //释放文件
                 img.Dispose();
@@ -246,10 +250,6 @@ namespace GIF_Compress
                 new_imgs.Dispose();
                 g_new_img.Dispose();
                 g_new_imgs.Dispose();
-            }
-            else
-            {
-                File.Copy(gifInPath, savePath);
             }
         }
 
@@ -285,8 +285,6 @@ namespace GIF_Compress
                             break;
                         }
                     }
-
-
                 }
                 return delay;
             }
